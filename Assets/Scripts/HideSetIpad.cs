543f566016f4f0acb224df5a345b5f7b9b45f8f5
Ivan Rodionov <still56789@gmail.com>
2021-05-15 01:02:53 +0500

    добавленна камера на планшет и взаимодействие с каждым вентилем

---
+++ Assets/Scripts/HideSetIpad.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideSetIpad : MonoBehaviour
+{
+    public GameObject ipad;
+    public void HideIpad()
+    {
+        ipad.SetActive(true);
+    }
+    public void SetIpad()
+    {
+
+    }
+}

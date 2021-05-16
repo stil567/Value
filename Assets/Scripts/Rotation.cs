28e4881175092dc1d2fa6f8108ec91fc6922b22a
Ivan Rodionov <still56789@gmail.com>
2021-05-16 22:02:43 +0500

    добавленно изменение уровня воды в баке при открытии вентилей

--- Assets/Scripts/Rotation.cs
+++ Assets/Scripts/Rotation.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class Rotation : MonoBehaviour
 {
+    public GameObject Fluid;
     public int Rotationoffset = 10;//скорость поворота
     public float rotz;
+
+    private float heightFluid = 0.1f;
 
     private Vector3 mousePosition;
 
     void Update()
     {
-        
+        if (transform.rotation.y > 0)
+        {
+            heightFluid += 0.0001f;
+            Fluid.transform.localScale = new Vector3(1f, heightFluid, 1f);
+        }
     }
 
     void OnTriggerStay(Collider other)
@@ -31,8 +38,8 @@ public class Rotation : MonoBehaviour
         Vector3 difference = mousePosition - transform.position;
         difference.Normalize();
         //  угол поворота
-        rotz = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        rotz = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg * Rotationoffset;
         // Применяем поворот вокруг оси y
-        transform.rotation = Quaternion.Euler(0f, rotz * Rotationoffset, 0f);
+        transform.rotation = Quaternion.Euler(0f, rotz , 0f);
     }
 }
